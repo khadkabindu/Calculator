5f120de6208708dc3fe1d9ad368adc58a7bee8f0
Bindu Khadka <bindu.khadka.np@gmail.com>
2023-09-25 16:45:11 +0545

    add function to evaluate string

---
+++ script.js
@@ -0,0 +1,28 @@
+function clearResult(){
+
+    const result = document.getElementById("display");
+    result.value = '';
+} 
+
+
+function appendNumber(value){
+    const input = document.getElementById("display");
+    input.value += value;
+}
+
+function appendOperator(operator){
+    const input = document.getElementById('display');
+    if (input.value === '') {
+        return; 
+    }
+    input.value += operator;
+}
+
+function calculateResult(){
+    const resultInput = document.getElementById('display');
+    try {
+        resultInput.value = eval(resultInput.value);
+    } catch (error) {
+        resultInput.value = 'Error';
+    }
+}
